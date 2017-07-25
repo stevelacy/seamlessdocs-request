09388bbc7a5e7ec5570c99094e3695c0bd927407
Steve Lacy <me@slacy.me>
2017-07-25 11:18:36 -0700

    update

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const hmacSHA256 = require('crypto-js/hmac-sha256')
 const timestamp = String(new Date().getTime()).substring(0, 10)
 const apiKey = process.argv[2]
 const secret = process.argv[3]
-const payload = 'GET+/form/CO17051000028335459/elements+' + timestamp
+const param = 'feed'
+const payload = 'GET+/form/CO17051000028335459/' + param + '+' + timestamp
 
 const signature = hmacSHA256(payload, secret).toString()
 
-request.get('https://jerseycitynj.seamlessdocs.com/api/form/CO17051000028335459/elements')
-  .set('Authorization', 'HMAC-SHA256 api_key=' + apiKey +' Signature=' + signature)
-  .set('Date', timestamp)
+request.get('https://jerseycitynj.seamlessdocs.com/api/form/CO17051000028335459/' + param)
+  .set({
+    Authorization: 'HMAC-SHA256 api_key=' + apiKey +' Signature=' + signature,
+    Date: timestamp
+  })
   .end(function (err, res) {
-    console.log(err, res.body)
+    console.log(JSON.stringify(res.body, null, 2))
   })
